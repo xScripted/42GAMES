b969910c73c751dd0338e9d3b1dae36491ca407f
sergio <sergiolamorda@gmail.com>
2018-07-29 21:41:24 +0200

    Formulario Login

---
+++ public/js/custom/login.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+    const loginGoogle = document.getElementById('loginGoogle');
+    const loginFacebook = document.getElementById('loginFacebook');
+
+    loginGoogle.addEventListener('click', event => {
+        console.log('Hellouda');
+    });
+
+    loginFacebook.addEventListener('click', event => {
+        console.log('Helloud2');
+    });
+}
